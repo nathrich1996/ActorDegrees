07f7e3e6e815aa7aee7e3f2f00421e90af92a6da
mattcamarena <camarena.matthew@gmail.com>
2021-08-25 22:15:24 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-
+fetch("https://data-imdb1.p.rapidapi.com/series/id/tt0108778/cast/", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
+        "x-rapidapi-key": "b871d38f13msh1e3945195ee01d1p1d8da7jsn2424d81f7ee1"
+    }
+})
+.then(response => {
+    console.log(response);
+})
+.catch(err => {
+    console.error(err);
+});
