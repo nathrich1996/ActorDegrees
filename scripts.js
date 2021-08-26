1c036f7c19a9c1f2bf2efb57a02dc4f063bfc98b
mattcamarena <camarena.matthew@gmail.com>
2021-08-25 22:19:45 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,20 @@
 
 
 function startDegrees(){
-  document.getElementById("res").innerHTML = rot13(document.getElementById("textBox").value.toUpperCase());
-    
+
+    fetch("https://data-imdb1.p.rapidapi.com/actor/imdb_id_byName/Pacino/", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
+        "x-rapidapi-key": "b871d38f13msh1e3945195ee01d1p1d8da7jsn2424d81f7ee1"
+    }
+})
+.then(response => {
+    console.log(response);
+})
+.catch(err => {
+    console.error(err);
+});
 }
 
 
